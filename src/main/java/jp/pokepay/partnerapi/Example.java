63ebbcbacb15591f6bd19d95f1679384507eeba4
cxxxr <g23tlm@gmail.com>
2019-08-21 14:57:01 +0900

    tweak

--- src/main/java/jp/pokepay/partnerapi/Example.java
+++ src/main/java/jp/pokepay/partnerapi/Example.java
@@ -1,27 +1,22 @@
 package jp.pokepay.partnerapi;
 
 import jp.pokepay.partnerapi.request.CreateCheck;
-import jp.pokepay.partnerapi.request.CreateTransaction;
 import jp.pokepay.partnerapi.request.Request;
 import jp.pokepay.partnerapi.response.Check;
-import jp.pokepay.partnerapi.response.Pong;
-import jp.pokepay.partnerapi.response.Transaction;
 
 import java.io.File;
 
 public class Example {
     public static void main(String[] args) {
         try {
-            PartnerAPI client = new PartnerAPI(new File("./config.properties"));
-            try {
-                Request request = new CreateCheck("f785442d-e81d-453e-acdd-dc0716e4051f")
-                        .setAmount(1);
-                Check check = (Check) client.send(request);
-                System.out.println(check.getMoneyAmount());
-            } catch (BankHttpError error) {
-                System.out.println(error.getType());
-                System.out.println(error.getMessage());
-            }
+            PartnerAPI client = new PartnerAPI(new File("/Users/user/.pokepay/config.properties"));
+            Request request = new CreateCheck("c3ee6f56-b8f1-4659-9410-503cbe234462")
+                    .setAmount(1);
+            Check check = (Check) client.send(request);
+            System.out.println(check.getId());
+        } catch (BankHttpError error) {
+            System.out.println(error.getType());
+            System.out.println(error.getMessage());
         } catch (Exception ex) {
             ex.printStackTrace();
         }
