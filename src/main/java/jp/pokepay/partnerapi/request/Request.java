c65c0efb756ce1f1c01257537a9bf0cf25eaea05
cxxxr <g23tlm@gmail.com>
2020-07-09 14:34:53 +0900

    add

--- src/main/java/jp/pokepay/partnerapi/request/Request.java
+++ src/main/java/jp/pokepay/partnerapi/request/Request.java
@@ -1,9 +1,13 @@
 package jp.pokepay.partnerapi.request;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSerializationContext;
+
 import java.lang.reflect.Type;
 
 public abstract class Request {
     public abstract Method method();
     public abstract String path();
     public abstract Type getResponseClass();
+    public abstract JsonObject getRequestBodyJsonObject(JsonSerializationContext context);
 }
